bbcbf84ec34ba7e937eafdd1ee6e79fb6c73674a
jt-junichi-nishimura <dihd.1118@icloud.com>
2021-07-04 19:12:29 +0900

    Initial Commit

---
+++ src/main/java/com/jt/project/sake/JtProjectSakeApplication.java
@@ -0,0 +1,13 @@
+package com.jt.project.sake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtProjectSakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtProjectSakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jt/project/sake/JtProjectSakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt.project.sake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtProjectSakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
